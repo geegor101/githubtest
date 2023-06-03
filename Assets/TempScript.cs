0af703a50af496b6133b4c4aabdd576cd405a610
geegor101 <72266539+geegor101@users.noreply.github.com>
2023-06-03 16:19:02 -0500

    Hello from RIDER!

--- Assets/TempScript.cs
+++ Assets/TempScript.cs
@@ -7,7 +7,7 @@ public class TempScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Startup
     }
 
     // Update is called once per frame
