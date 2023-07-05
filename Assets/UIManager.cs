6edd2ad611a90fbdfe4ccb7b1439dfe6eed7e016
geegor101 <72266539+geegor101@users.noreply.github.com>
2023-07-05 13:31:21 -0500

    UI

---
+++ Assets/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<UIDocument>().rootVisualElement.Q<DropdownField>()
+    }
+
+    // Update is called once per frame
+    
+}
