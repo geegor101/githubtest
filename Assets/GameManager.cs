9e33b73cbb4253673c9ab266dc5a022f7ea09b15
geegor101 <72266539+geegor101@users.noreply.github.com>
2023-07-08 01:59:41 -0500

    Context Actions Setup

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,30 +1,158 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     
-    [SerializeField]
-    private Camera _camera;
-    [SerializeField]
-    private AudioListener _listener;
+
+    /*
+     * Use these to determine validity of taken actions
+     * you can read/write to Gamestatus if you want to save data
+     */
     
+    /// Add any status effects here about past actions
+    public static readonly Dictionary<string, string> gameStatus = new Dictionary<string, string>();
+    /// Reference any previous inputs given by the player
+    public static readonly List<TurnInput> pastActions = new List<TurnInput>();
+
     void Start()
     {
-        
-
-        
-
+        LoadContextActions();
     }
-
-    private void Update()
+    
+    public void TakeTurn(TalkInput talkInput, ActionInput actionInput)
     {
-        //SceneManager.GetActiveScene()
+        ResponseActionCallback callback = new ResponseActionCallback();
+        TurnInput turnInput = new TurnInput(talkInput, actionInput);
+        GatherResponsesEvent.Invoke(turnInput, callback);
+        pastActions.Add(turnInput);
+        IEnumerable<ResponseAction> responseActions = callback.GetResponseActions().Take(2);
+        if (responseActions.Count() <= 0)
+            return; //TODO Call default action here
         
     }
+    public enum TalkInput
+    {
+        IGNORE,
+        ITSNOTYOU,
+        IHATEYOU,
+        ILOVEYOU
+    }
+    
+    public enum ActionInput
+    {
+        NOTHING,
+        STUDY,
+        SPENDTIME,
+        STRIPPERS
+    }
+    
+    /**
+     * Extend this class to make new responses
+     */
+    [ResponseAction]
+    public abstract class ResponseAction
+    {
+        private float _weightCache;
+        public void CalcWeight(TurnInput input, ResponseActionCallback callback)
+        {
+            _weightCache = CalcWeightInternal(input);
+            if (_weightCache > 0)
+                callback.AddViableResponse(this);
+        }
+
+        /**
+         * Use the input to calculate the weighting of this action. return a negative value if it is not valid
+         */
+        public abstract float CalcWeightInternal(TurnInput input);
+
+        public float getWeightCache()
+        {
+            return _weightCache;
+        }
+        
+        /**
+         * What to call when this action is chosen as the desired action
+         */
+        public abstract void DoAction(TurnInput input);
+    }
+
+    #region Internal
+
+    public struct TurnInput
+    {
+        public TalkInput TalkInput { get; private set; }
+        public ActionInput ActionInput { get; private set; }
+
+        public TurnInput(TalkInput talkInput, ActionInput actionInput)
+        {
+            TalkInput = talkInput;
+            ActionInput = actionInput;
+        }
+    }
+
+    private void LoadContextActions()
+    {
+        foreach (Type type in Assembly.GetExecutingAssembly().GetTypes().Where(type => type.IsDefined(typeof(ResponseActionAttribute))))
+        {
+            if (type == typeof(ResponseAction))
+                return;
+            GatherResponsesEvent += ((ResponseAction)Activator.CreateInstance(type)).CalcWeight;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ResponseActionAttribute : Attribute
+    {
+        public ResponseActionAttribute()
+        {
+        }
+    }
+
+    private class ResponseComparer : IComparer<ResponseAction>
+    {
+        public static readonly ResponseComparer _Comparer = new ResponseComparer();
+        
+        private ResponseComparer(){}
+        
+        public int Compare(ResponseAction x, ResponseAction y)
+        {
+            if (x == null || y == null)
+                return 0;
+            return (int) (x.getWeightCache() - y.getWeightCache());
+        }
+    }
+
+    public delegate void ResponseActionDelegate(TurnInput input, ResponseActionCallback callback);
+
+    public static event ResponseActionDelegate GatherResponsesEvent;
+
+    public class ResponseActionCallback
+    {
+        private readonly SortedSet<ResponseAction> viableResponses = new SortedSet<ResponseAction>(ResponseComparer._Comparer);
+
+        public ResponseActionCallback()
+        {
+            
+        }
+
+        public void AddViableResponse(ResponseAction action)
+        {
+            viableResponses.Add(action);
+        }
+
+        public SortedSet<ResponseAction> GetResponseActions()
+        {
+            return viableResponses;
+        }
+    }
+
+    #endregion
+    
     
 }
