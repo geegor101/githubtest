2e36d2a622e3b7424f34a8b5523af3bf7eb04a47
Daniel <dandoodle956@gmail.com>
2023-07-08 18:33:20 -0500

    Added some logic

--- Assets/GoodGirlFriendRespones.cs
+++ Assets/GoodGirlFriendRespones.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 using static GameManager;
 
-
-public class TalkAction : ResponseAction
+#region GoodGirlFriendResponses
+public class smallHate : ResponseAction
 {
     protected override float CalcWeightInternal(TurnInput input)
     {
@@ -20,3 +20,127 @@ public class TalkAction : ResponseAction
         
     }
 }
+
+public class smallLove : ResponseAction
+{
+    protected override float CalcWeightInternal(TurnInput input)
+    {
+        // Calculate the weight based on the TalkInput and ActionInput
+        // Return a negative value if the action is not valid for the given input
+        float weight = -5;
+        var talkInput = input.TalkInput;
+        var actionInput = input.ActionInput;
+        
+        if( talkInput == TalkInput.TALKC && actionInput == ActionInput.ACTIONA)
+        {
+            weight *= -1;
+        }
+        else if (talkInput == TalkInput.TALKB && actionInput == ActionInput.ACTIONB)
+        {
+            weight *= -1;
+        }
+        else if (talkInput == TalkInput.TALKB && actionInput == ActionInput.ACTIONC)
+        {
+            weight *= -1;
+        }
+        else if (talkInput == TalkInput.TALKD && actionInput == ActionInput.ACTIONB)
+        {
+            weight *= -1;
+        }
+        else if (talkInput == TalkInput.TALKD && actionInput == ActionInput.ACTIOND)
+        {
+            weight *= -1;
+        }
+
+        return weight;
+    }
+
+    public override void DoAction(TurnInput input)
+    {
+        
+    }
+}
+
+public class mediumHate : ResponseAction
+{
+    protected override float CalcWeightInternal(TurnInput input)
+    {
+        // Calculate the weight based on the TalkInput and ActionInput
+        // Return a negative value if the action is not valid for the given input
+        float weight = -10;
+        
+        List<TurnInput> goodInputs = new List<TurnInput>();
+        
+        goodInputs.Add(new TurnInput(TalkInput.TALKB, ActionInput.ACTIONA));
+        goodInputs.Add(new TurnInput(TalkInput.TALKD, ActionInput.ACTIONA));
+        goodInputs.Add(new TurnInput(TalkInput.TALKA, ActionInput.ACTIONC));
+        goodInputs.Add(new TurnInput(TalkInput.TALKC, ActionInput.ACTIONC));
+        goodInputs.Add(new TurnInput(TalkInput.TALKD, ActionInput.ACTIONC));
+        goodInputs.Add(new TurnInput(TalkInput.TALKA, ActionInput.ACTIOND));
+        goodInputs.Add(new TurnInput(TalkInput.TALKB, ActionInput.ACTIOND));
+        goodInputs.Add(new TurnInput(TalkInput.TALKC, ActionInput.ACTIOND));
+        
+        if( goodInputs.Contains(input))
+        {
+            weight *= -1;
+        }
+        
+        return weight;
+    }
+
+    public override void DoAction(TurnInput input)
+    {
+        
+    }
+}
+
+public class mediumLove : ResponseAction
+{
+    protected override float CalcWeightInternal(TurnInput input)
+    {
+        // Calculate the weight based on the TalkInput and ActionInput
+        // Return a negative value if the action is not valid for the given input
+        float weight = 10;
+        return weight;
+    }
+
+    public override void DoAction(TurnInput input)
+    {
+        
+    }
+}
+
+public class bigHate : ResponseAction
+{
+    protected override float CalcWeightInternal(TurnInput input)
+    {
+        // Calculate the weight based on the TalkInput and ActionInput
+        // Return a negative value if the action is not valid for the given input
+        float weight = 20;
+        return weight;
+    }
+
+    public override void DoAction(TurnInput input)
+    {
+        
+    }
+}
+
+public class bigLove : ResponseAction
+{
+    protected override float CalcWeightInternal(TurnInput input)
+    {
+        // Calculate the weight based on the TalkInput and ActionInput
+        // Return a negative value if the action is not valid for the given input
+        float weight = 20;
+        return weight;
+    }
+
+    public override void DoAction(TurnInput input)
+    {
+        
+    }
+}
+
+
+#endregion
--- Assets/Responses/SampleResponses.cs
+++ Assets/Responses/SampleResponses.cs
@@ -19,7 +19,5 @@ namespace DefaultNamespace.Responses
         }
     }
     
-    
-
     #endregion
 }
